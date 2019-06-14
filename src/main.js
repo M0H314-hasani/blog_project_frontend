dc650dcae87a1f60aea15a8396761b96826f04a7
M0H314-hasani <m0h314.hasani@gmail.com>
2019-06-15 03:23:19 +0430

    Laying out the project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import ApiService from "./common/api.service";
+import ErrorFilter from "./common/error.filter";
+
 Vue.config.productionTip = false;
+
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.filter("error", ErrorFilter);
+
+ApiService.init();
+
+// TODO: Ensure we checked auth before each page load.
 
 new Vue({
   router,
