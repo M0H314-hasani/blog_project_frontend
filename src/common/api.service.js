264f7afdebe9d47cefda0f0fbb748a7f5ed60332
M0H314-hasani <m0h314.hasani@gmail.com>
2019-06-15 20:47:25 +0430

    Implement api services done!

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -53,4 +53,94 @@ const ApiService = {
 
 export default ApiService;
 
-//TODO: Implement api services.
+// Api services.
+
+export const UserService = {
+  index(user) {
+    return ApiService.get(`user/${user}/index`);
+  },
+  register(params) {
+    return ApiService.post(`user/register`, params);
+  },
+  updateUserInfo(params) {
+    return ApiService.patch(`user`, params);
+  },
+  uploadAvatar(params) {
+    return ApiService.post(`user/avatar/upload`, params);
+  },
+  bookmarkedPosts() {
+    return ApiService.get(`user/post/bookmarks`);
+  },
+  likedPosts() {
+    return ApiService.get(`user/post/likes`);
+  },
+  followedCollections() {
+    return ApiService.get(`user/collections/followed`);
+  },
+  postRetrieveUserCollectionsOriented() {
+    return ApiService.get(`user/collections/followed/posts`);
+  },
+  userCollections() {
+    return ApiService.get(`user/collections`);
+  },
+  follow(user) {
+    return ApiService.get(`user/${user}/follow`);
+  },
+  followers(user) {
+    return ApiService.get(`user/${user}/followers`);
+  },
+  following(user) {
+    return ApiService.get(`user/${user}/following`);
+  }
+};
+
+export const CollectionService = {
+  index() {
+    return ApiService.get(`collection`);
+  },
+  create(params) {
+    return ApiService.post(`collection`, params);
+  },
+  destroy(collection) {
+    return ApiService.delete(`collection/${collection}`);
+  },
+  postRetrieveCollectionOriented(collection) {
+    return ApiService.get(`collection/${collection}/posts`);
+  },
+  followCollection(collection) {
+    return ApiService.get(`collection/${collection}/follow`);
+  }
+};
+
+export const PostService = {
+  index() {
+    return ApiService.get(`post`);
+  },
+  create(params) {
+    return ApiService.post(`post`, params);
+  },
+  relatedPosts(post, quantity) {
+    return ApiService.get(`post/${post}/related/${quantity}`);
+  },
+  uploadFeaturedImage(post, params) {
+    return ApiService.post(`post/${post}/featured-image/upload`, params);
+  },
+  changeStatus(post, params) {
+    return ApiService.patch(`post/${post}/status`, params);
+  },
+  changeAccessibility(post, params) {
+    return ApiService.patch(`post/${post}/accessibility`, params);
+  },
+  update(post, params) {
+    return ApiService.patch(`post/${post}`, params);
+  },
+  show(post) {
+    return ApiService.get(`post/${post}`);
+  },
+  bookmarkPost(post) {
+    return ApiService.get(`post/${post}/bookmark`);
+  },
+  likePost(post) {
+    return ApiService.get(`post/${post}/like`);
+  }
+};
